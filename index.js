6f9c1485b1744d0077832f9bd3c1b448a11d2243
JULIA VITORIA ZIMMERMANN <4544358409@estudante.cedupguaramirim.com.br>
2025-08-21 08:27:11 -0300

    atualizando

--- index.js
+++ index.js
@@ -606,60 +606,57 @@ app.get('/atualizar/:id_pagamento', (req, res) => {
     });
 });
 
-app.get('/pagamentos', (req, res) => {
-    res.sendFile(__dirname + '/pagamentos.html');
+app.get('/manutencao', (req, res) => {
+    res.sendFile(__dirname + '/manutencao.html');
 });
 
-app.post('/pagamentos',(req, res) => {
-    const id_pagamento = req.body.id_pagamento;
-    const id_morador = req.body.id_morador;
-    const id_referencia = req.body.id_referencia;
-    const data_pagamento = req.body.data_pagamento;
-    const valor_pago = req.body.valor_pago;
+app.post('/manutencao',(req, res) => {
+    const id_manutencao = req.body.id_manutencao;
+    const id_tipo = req.body.id_tipo;
+    const data0 = req.body.data0;
+    const locall = req.body.locall;
 
     const insert = 
-        'INSERT INTO Pagamento (id_pagamento, id_morador, id_referencia, data_pagamento, valor_pago) VALUES (?, ?, ?, ?, ?)';
+        'INSERT INTO Manutencao (id_manutencao, id_tipo, data0, locall) VALUES (?, ?, ?, ?)';
 
-    connection.query(insert, [id_pagamento, id_morador, id_referencia, data_pagamento, valor_pago], (err, results) => {
+    connection.query(insert, [id_manutencao, id_tipo, data0, locall], (err, results) => {
         if (err) {
-            console.error("Erro ao cadastrar pagamento: ", err);
-            res.status(500).send('Erro ao cadastrar pagamento');
+            console.error("Erro ao cadastrar manutenção: ", err);
+            res.status(500).send('Erro ao cadastrar manutenção');
             return;
         } else{
-            console.log("Pagamento cadastrado com sucesso");
+            console.log("Manutenção cadastrada com sucesso");
             res.redirect('/');
         }
     });
 });
 
-app.get('/pagamento', (req, res) => {
-    const select = 'SELECT * FROM Pagamento';
+app.get('/manutencao', (req, res) => {
+    const select = 'SELECT * FROM Manutencao';
  
     connection.query(select, (err, rows) => {
         if (err) {
-            console.error("Erro listar pagamentos: ", err);
-            res.status(500).send('Erro ao listar pagamentos');
+            console.error("Erro listar manutenções: ", err);
+            res.status(500).send('Erro ao listar manutenções');
             return;
         } else {
-            console.log("Pagamentos listados com sucesso");
+            console.log("Manutenções listadas com sucesso");
             res.send(`
-                <h1>Dados do Pagamento</h1>
+                <h1>Dados da Manutenção</h1>
                 <table border="1">
                     <tr>
-                        <th>ID Pagamento</th>
-                        <th>ID Morador</th>
-                        <th>ID Referência</th>
-                        <th>Data do Pagamento</th>
-                        <th>Valor Pago</th>
+                        <th>ID Manutenção</th>
+                        <th>ID Tipo</th>
+                        <th>Data da Manutenção</th>
+                        <th>Local da Manutenção</th>
                     </tr>
                     ${rows.map(row => `
                         <tr>
-                            <td>${row.id_pagamento}</td>
-                            <td>${row.id_morador}</td>
-                            <td>${row.id_referencia}</td>
-                            <td>${row.data_pagamento}</td>
-                            <td>${row.valor_pago}</td>
-                            <td><a href= "/deletar/${row.id_pagamento}">Deletar</a></td>
+                            <td>${row.id_manutencao}</td>
+                            <td>${row.id_tipo}</td>
+                            <td>${row.data0}</td>
+                            <td>${row.locall}</td>
+                            <td><a href= "/deletar/${row.id_manutencao}">Deletar</a></td>
                         </tr>`).join('')}
                 </table>
                 <a href="/">Voltar</a>
@@ -668,57 +665,58 @@ app.get('/pagamento', (req, res) => {
     });
 });
 
-app.get('/deletar/:id_pagamento', (req, res) => {
-    const id_pagamento = req.params.id_pagamento;
-    const deletar = 'DELETE FROM Pagamento WHERE id_pagamento = ?';
-    connection.query(deletar, [id_pagamento], (err, results) => {
+app.get('/deletar/:id_manutencao', (req, res) => {
+    const id_manutencao = req.params.id_manutencao;
+    const deletar = 'DELETE FROM Manutencao WHERE id_manutencao = ?';
+    connection.query(deletar, [id_manutencao], (err, results) => {
         if (err) {
-            console.error("Erro ao deletar pagamento: ", err);
-            res.status(500).send('Erro ao deletar pagamento');
+            console.error("Erro ao deletar manutenção: ", err);
+            res.status(500).send('Erro ao deletar manutenção');
             return;
         } else {
-            console.log("Pagamento deletado com sucesso");
-            res.redirect('/pagamentos');
+            console.log("Manutenção deletada com sucesso");
+            res.redirect('/manutencao');
         }
     });
 });
 
-app.get('/atualizar/:id_pagamento', (req, res) => {
-    const id_pagamento = req.params.id_pagamento;
-    const select = 'SELECT * FROM Pagamento WHERE id_pagamento = ?';
 
-    connection.query(select, [id_pagamento], (err, rows) => {
+app.get('/atualizar/:id_manutencao', (req, res) => {
+    const id_manutencao = req.params.id_manutencao;
+    const select = 'SELECT * FROM Manutencao WHERE id_manutencao = ?';
+
+    connection.query(select, [id_manutencao], (err, rows) => {
         if(!err && rows.length > 0) {
-            const pagamento = rows[0];
+            const manutencao = rows[0];
             res.send(`
                 <html>
                     <head>
-                        <tittle>Atualizar Pagamentos</h1>
-                        <form action="/atualizar/${pagamento.id_pagamento}" method="POST">
-                            <label for="id_pagamento">Id Pagamento:</label>
-                            <input type="text" id="id_pagamento" name="id_pagamento"
-                            value="${pagamento.id_pagamento}" required><br><br>
+                        <tittle>Atualizar Manutenções</h1>
+                        <form action="/atualizar/${manutencao.id_manutencao}" method="POST">
+                            <label for="id_manutencao">Id Manurtenção:</label>
+                            <input type="text" id="id_manutencao" name="id_manutencao"
+                            value="${manutencao.id_manutencao}" required><br><br>
 
-                            <label for="id_referencia">ID da Referência:></label>
-                            <input type="text" id="id_referencia" name="id_referencia"
-                            value="${pagamento.id_referencia}" required><br><br>
+                            <label for="id_tipo">Tipo da Manutenção(ID):></label>
+                            <input type="text" id="id_tipo" name="id_tipo"
+                            value="${manutencao.id_tipo}" required><br><br>
 
-                            <label for="data_pagamento">Data do Pagamento:</label>
-                            <input type="text" id="data_pagamento" name="data_pagamento"
-                            value="${pagamento.data_pagamento}" required><br><br>
+                            <label for="data0">Data da Manutenção:</label>
+                            <input type="text" id="data0" name="data0"
+                            value="${manutencao.data_pagamento}" required><br><br>
 
-                            <label for="valor_pago">Valor Pago:</label>
-                             <input type="number" step="0.01" id="valor_pago" name="valor_pago"
-                            value="${pagamento.valor_pago}" required><br><br>
+                            <label for="locall">Local Manutenção:</label>
+                             <input type="text" id="locall" name="locall"
+                            value="${manutencao.locall}" required><br><br>
 
                                 <input type="submit" value="Atualizar">
                         </form>
-                        <a href="/pagamento">Voltar</a>
+                        <a href="/manutencao">Voltar</a>
                     </body>
                 </html>
                 `);
         } else {
-            res.status(404).send('Pagamento não encontrado');
+            res.status(404).send('Manutenção não encontrada');
         }
     });
 });
